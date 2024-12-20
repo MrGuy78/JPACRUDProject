b21f0c757df8c76e18f3f50dcf7ab19d06690b55
Andrew Nelson <nels4766@gmail.com>
2024-12-20 16:05:40 -0700

    spring boot, gradle stuff

---
+++ MountainsClimbedByMe/src/main/java/com/skilldistillery/mountain/MountainsClimbedByMeApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mountain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainsClimbedByMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainsClimbedByMeApplication.class, args);
+	}
+
+}
